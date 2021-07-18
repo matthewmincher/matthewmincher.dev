3fdc6ed7c462d36a4933e597ea067263b3f6672f
Matthew Mincher <matthew@mincher.org>
2021-07-18 21:59:57 +0100

    Progress on home page

---
+++ src/data/aroundtheweb.js
@@ -0,0 +1,45 @@
+import { faStackOverflow, faGithub, faTwitter, faInstagram, faLastfm, faGoodreadsG, faSteam } from '@fortawesome/free-brands-svg-icons'
+
+const data = {
+	professional: [
+		{
+			link: 'https://stackoverflow.com/users/590487/matt',
+			label: 'Stack Overflow',
+			icon: faStackOverflow
+		},
+		{
+			link: 'https://github.com/matthewmincher',
+			label: 'Github',
+			icon: faGithub
+		}
+	],
+	personal: [
+		{
+			link: 'https://twitter.com/matthewmincher',
+			label: 'Twitter',
+			icon: faTwitter
+		},
+		{
+			link: 'https://instagram.com/matthewmincher',
+			label: 'Instagram',
+			icon: faInstagram
+		},
+		{
+			link: 'https://www.last.fm/user/matthewmincher',
+			label: 'Last.fm',
+			icon: faLastfm
+		},
+		{
+			link: 'https://www.goodreads.com/user/show/22574656-matthew',
+			label: 'Goodreads',
+			icon: faGoodreadsG
+		},
+		{
+			link: 'https://steamcommunity.com/id/hartshill/',
+			label: 'Steam',
+			icon: faSteam
+		}
+	]
+}
+
+export default data
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,14 @@ import * as React from "react"
 import Layout from "../components/layout";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as Styles from "./index.module.scss"
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
+import PhpLogo from "../images/logo-php.svg"
+import MysqlLogo from "../images/logo-mysql.svg"
+import NodejsLogo from "../images/logo-nodejs.svg"
 
-import { faStackOverflow, faGithub, faTwitter, faInstagram, faLastfm, faGoodreadsG, faSteam } from '@fortawesome/free-brands-svg-icons'
+import MapImage from "../images/map.png"
+import contactData from "../data/aroundtheweb"
 
 
 
@@ -17,21 +22,48 @@ const IndexPage = () => {
 			<h1 className={Styles.heroTitle}>Matthew Mincher</h1>
 			<div className={Styles.heroSubtitle}>Software Engineer at <a href="https://www.codefanatics.co.uk" target="_blank" rel="noreferrer">Code Fanatics</a>. Full Stack Developer.</div>
 
-			<div className={Styles.linksContainer}>
-				<div className="constrainedContent">
-					<h2>Around the web</h2>
-
-					<div className={Styles.professional}>
-						<a title="Stackoverflow" href="https://stackoverflow.com/users/590487/matt" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faStackOverflow} size="4x" /></a>
-						<a title="Github" href="https://github.com/matthewmincher" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size="4x" /></a>
+			<div className="constrainedContent">
+				<div className={Styles.skillsSection + ' ' + Styles.server}>
+					<div className={Styles.item}>
+						<div className={Styles.identity}>
+							<img src={PhpLogo} height="65" alt="PHP" />
+						</div>
 
 					</div>
-					<div className={Styles.personal}>
-						<a title="Twitter" href="https://twitter.com/matthewmincher" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} size="2x" /></a>
-						<a title="Instagram" href="https://instagram.com/matthewmincher" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
-						<a title="Last.fm" href="https://www.last.fm/user/matthewmincher" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLastfm} size="2x" /></a>
-						<a title="Goodreads" href="https://www.goodreads.com/user/show/22574656-matthew" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGoodreadsG} size="2x" /></a>
-						<a title="Steam" href="https://steamcommunity.com/id/hartshill/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faSteam} size="2x" /></a>
+					<div className={Styles.item}>
+						<div className={Styles.identity}>
+							<img src={MysqlLogo} height="65" alt="mySQL" />
+						</div>
+					</div>
+
+					<div className={Styles.item}>
+						<div className={Styles.identity}>
+							<img src={NodejsLogo} height="65" alt="nodeJS" />
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			<div className={Styles.linksContainer}>
+				<div className="constrainedContent">
+					<div className={Styles.positionContainer}>
+						<h2>Around the web</h2>
+
+						<div className={Styles.map}>
+							<img alt="Based in Chester, UK" src={MapImage} />
+						</div>
+
+						<div className={Styles.professional}>
+							{contactData.professional.map((link) => (
+								<a key={link.label} title={link.label} aria-label={link.label} href={link.link} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={link.icon} size="4x" /></a>
+							))}
+						</div>
+						<div className={Styles.personal}>
+							{contactData.personal.map((link) => (
+								<a key={link.label} title={link.label} aria-label={link.label} href={link.link} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={link.icon} size="2x" /></a>
+							))}
+						</div>
 					</div>
 				</div>
 			</div>
