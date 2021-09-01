e6b94a17a699225482a7a5211d6a870e7f6882e9
Matthew Mincher <matthew@mincher.org>
2021-09-02 00:25:02 +0100

    Remove google analytics

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,10 +21,6 @@ const Layout = ({ pageTitle, children }) => {
                 </div>
                 <Footer />
             </main>
-
-            <script>
-
-            </script>
         </>
     )
 }
